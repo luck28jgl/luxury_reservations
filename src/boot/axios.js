5e19ee6f4d9bc4e712845e54e9a53df4f39b4d60
JGL <luckibarra15@gmail.com>
2025-04-20 06:30:47 -0600

    cambios masibos baken primero 1.10

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // const URL_API = 'http://127.0.0.1:8000'
 // const URL_API = 'https://docs.djangoproject.com/en/5.2/howto/deployment'
 const URL_API = process.env.NODE_ENV === 'production'
-    ? 'https://docs.djangoproject.com/en/5.2/howto/deployment/'  // URL de tu backend en producción
+    ? 'https://docs.djangoproject.com/en/5.2/howto/deployment'  // URL de tu backend en producción
     : 'http://127.0.0.1:8000';  // URL de tu backend en desarrollo
 
 const axiosInstance = axios.create({
