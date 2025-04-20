84404cb56a8babfaca5de2f0f7cf1a6917a3c33e
JGL <luckibarra15@gmail.com>
2025-04-20 06:29:24 -0600

    cambios masibos baken primero 1.7

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 // import cryptoJs from 'crypto-js';
 
 // const URL_API = 'http://127.0.0.1:8000'
-const URL_API = 'https://docs.djangoproject.com/en/5.2/howto/deployment'
+// const URL_API = 'https://docs.djangoproject.com/en/5.2/howto/deployment'
+const URL_API = process.env.NODE_ENV === 'production'
+    ? 'https://docs.djangoproject.com/en/5.2/howto/deployment/'  // URL de tu backend en producción
+    : 'http://127.0.0.1:8000';  // URL de tu backend en desarrollo
 
 const axiosInstance = axios.create({
     baseURL: URL_API + '/api',
