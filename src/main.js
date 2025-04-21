f6f661f0f778e4c8c1de7565770f0166e8b15e85
JGL <luckibarra15@gmail.com>
2025-04-21 06:30:02 -0600

    cambios sin error

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from '@/router/index.js'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/css/Index.css';
+import 'vue-loading-overlay/dist/css/index.css';
 import store from './store'; // Importa tu store de Vuex
 
 
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['vue-loading-overlay'], // Marca esta dependencia como externa
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
