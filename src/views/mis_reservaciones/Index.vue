53ca9551bd227e32680820c73495c4468a62293c
JGL <luckibarra15@gmail.com>
2025-05-23 11:51:25 -0600

    cambios nuevos nuevas adiciones tes

--- src/views/mis_reservaciones/Index.vue
+++ src/views/mis_reservaciones/Index.vue
@@ -467,7 +467,7 @@
             formatDate(dateString) {
                 const date = new Date(dateString);
                 const day = String(date.getDate()).padStart(2, '0');
-                const month = String(date.getMonth() + 1).padStart(2, '0');
+                const month = String(date.getMonth()).padStart(2, '0');
                 const year = date.getFullYear();
                 return `${day}/${month}/${year}`;
             },
